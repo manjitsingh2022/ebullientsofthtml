f487dad4ab86b8a7d2b111c4a58750bd34fbda5a
Sudhir Dadwal <sudhir.dadwal@gmail.com>
2023-06-07 20:51:45 +0530

    Update Footer.php

--- footer.php
+++ footer.php
@@ -2,57 +2,85 @@
       <footer class="site-footer style-1" id="footer">
         <img class="bg-shape1" src="images/home-banner/shape1.png" alt="" />
 
-        <div class="footer-top background-luminosity" style="background-image: url(images/background/bg1.jpg)">
+        <div
+          class="footer-top background-luminosity"
+          style="background-image: url(images/background/bg1.jpg)"
+        >
           <div class="container">
             <div class="row">
-              <div class="col-xl-4 col-lg-12 col-md-12">
+              <div
+                class="col-xl-4 col-lg-12 col-md-12 wow fadeInUp"
+                data-wow-delay="0.2s"
+              >
                 <div class="widget widget_about">
                   <div class="footer-logo logo-white">
-                    <a href="index.html"><img src="images/logo-white.png" alt="" /></a>
+                    <a href="index.html"
+                      ><img src="images/logo-white.png" alt="" width="80px"
+                    /></a>
                   </div>
-                  <!-- <p>
-                    CryptoZone is a Crypto & Crypto Website by DexignZone lorem
-                    ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et
-                  </p> -->
+                  <p></p>
                   <div class="dz-social-icon transparent space-10">
                     <ul>
                       <li>
-                        <a target="_blank" class="fab fa-facebook-f" href="https://www.facebook.com/"></a>
+                        <a
+                          target="_blank"
+                          class="fab fa-facebook-f"
+                          href="https://www.facebook.com/ebullientsoft"
+                        ></a>
                       </li>
                       <li>
-                        <a target="_blank" class="fab fa-instagram" href="https://www.instagram.com/"></a>
+                        <a
+                          target="_blank"
+                          class="fab fa-instagram"
+                          href="https://www.instagram.com/"
+                        ></a>
                       </li>
                       <li>
-                        <a target="_blank" class="fab fa-twitter" href="https://twitter.com/"></a>
+                        <a
+                          target="_blank"
+                          class="fab fa-linkedin"
+                          href="https://www.linkedin.com/in/ebullient-soft-818318158/"
+                        ></a>
                       </li>
                       <li>
-                        <a target="_blank" class="fab fa-youtube" href="https://youtube.com/"></a>
+                        <a
+                          target="_blank"
+                          class="fab fa-youtube"
+                          href="https://youtube.com/"
+                        ></a>
                       </li>
                     </ul>
                   </div>
                 </div>
               </div>
-              <div class="col-xl-2 col-lg-4 col-md-4 col-sm-6">
+              <div
+                class="col-xl-2 col-lg-4 col-md-4 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.4s"
+              >
                 <div class="widget widget_services">
                   <h4 class="widget-title">Other Links</h4>
                   <ul>
+                    <li><a href="index.html">Home</a></li>
                     <li><a href="about-us.html">About Us</a></li>
-                    <li><a href="pricing.html">Pricing Plan</a></li>
-                    <li><a href="blog-list.html">Blog List</a></li>
-                    <li><a href="blog-grid.html">Our Blogs</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="services.html">Services</a></li>
                     <li><a href="contact-us.html">Contact Us</a></li>
                   </ul>
                 </div>
               </div>
-              <div class="col-xl-3 col-lg-4 col-sm-6">
+              <div
+                class="col-xl-3 col-lg-4 col-sm-6 wow fadeInUp"
+                data-wow-delay="0.6s"
+              >
                 <div class="widget recent-posts-entry">
                   <h4 class="widget-title">Blog Posts</h4>
                   <div class="widget-post-bx">
                     <div class="widget-post clearfix">
                       <div class="dz-info">
                         <h6 class="title">
-                          <a href="speech-recognition.html">What is Speech Recognition? </a>
+                          <a href="blog-details.html"
+                            >What is cryptocurrency and how does it work.</a
+                          >
                         </h6>
                         <span class="post-date"> JUNE 18, 2022</span>
                       </div>
@@ -61,7 +89,9 @@
                     <div class="widget-post clearfix">
                       <div class="dz-info">
                         <h6 class="title">
-                          <a href="react-native-app.html">Develop AI-based Mobile App with React Native</a>
+                          <a href="blog-details.html"
+                            >A cryptocurrency is a digital currency.</a
+                          >
                         </h6>
                         <span class="post-date"> AUGUST 22, 2022</span>
                       </div>
@@ -69,17 +99,31 @@
                   </div>
                 </div>
               </div>
-              <div class="col-xl-3 col-lg-4 col-sm-12">
+              <div
+                class="col-xl-3 col-lg-4 col-sm-12 wow fadeInUp"
+                data-wow-delay="0.8s"
+              >
                 <div class="widget widget_locations">
                   <h4 class="widget-title">Locations</h4>
                   <div class="clearfix">
-                    <h6 class="title">Washington</h6>
-                    <p>1559 Alabama Ave SE, DC 20032, Washington, USA</p>
+                    <h6 class="title">India</h6>
+                    <p>F-244, Phase 8-B, Industrial Area, Mohali, 160055</p>
                     <img src="images/footer/world-map-with-flags1.png" alt="" />
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <!-- Footer Bottom Part -->
+        <div class="footer-bottom text-center">
+          <div class="container">
+            <span class="copyright-text"
+              >Copyright © 2022
+              <a href="https://ebullientsoft.com/" target="_blank"
+                >ebullientsoft</a
+              >. All rights reserved.</span
+            >
           </div>
         </div>
       </footer>
@@ -89,23 +133,17 @@
       <button class="scroltop icon-up" type="button">
         <i class="fas fa-arrow-up"></i>
       </button>
-      </div>
+    </div>
 
-      <!-- JAVASCRIPT FILES ========================================= -->
-      <script src="js/jquery.min.js"></script>
-      <!-- JQUERY.MIN JS -->
-      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-      <!-- WOW.JS -->
-      <script src="vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-      <script src="vendor/wow/wow.js"></script>
-      <!-- WOW.JS -->
-      <script src="js/custom.js"></script>
-      <!-- CUSTOM JS -->
-
-
-      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-      <script src="assets/js/jquery.quicksand.js"></script>
-      <script src="assets/js/script.js"></script>
-      </body>
-
-      </html>
+    <!-- JAVASCRIPT FILES ========================================= -->
+    <script src="js/jquery.min.js"></script>
+    <!-- JQUERY.MIN JS -->
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- WOW.JS -->
+    <script src="vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="vendor/wow/wow.js"></script>
+    <!-- WOW.JS -->
+    <script src="js/custom.js"></script>
+    <!-- CUSTOM JS -->
+  </body>
+</html>
