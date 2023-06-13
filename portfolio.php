5081d7a19a36b7adfec163d6adbc2a8a5348b1a0
Baljeet Singh <ebsbaljeet4298@gmail.com>
2023-06-13 14:23:01 +0530

    c

--- portfolio.php
+++ portfolio.php
@@ -27,185 +27,161 @@
     <!-- Banner End -->
 
     <section class="portfolio_area section_gap_top " id="portfolio">
-        <div class="container">
-            <!-- <div class="row">
-            <div class="col-lg-12">
-                <div class="main_title text-left">
-                    <h2>quality work <br>
-                        Recently done project </h2>
+                <div class="filters portfolio-filter">
+                    <ul>
+                        <li class="active" data-filter="*">ALL</li>
+                        <li data-filter=".react">REACT</li>
+                        <li data-filter=".cms">CMS</li>
+                        <li data-filter=".php">PHP FRAMEWORK </li>
+                        <li data-filter=".uix">UI/UX</li>
+                        <li data-filter=".wordpress">WORDPRESS</li>
+                    </ul>
                 </div>
-            </div>
-        </div> -->
-            <div class="filters portfolio-filter">
-                <ul>
-                    <li class="active" data-filter="*">ALL</li>
-                    <li data-filter=".react">REACT</li>
-                    <li data-filter=".cms">CMS</li>
-                    <li data-filter=".php">PHP FRAMEWORK </li>
-                    <li data-filter=".uix">UI/UX</li>
-                    <li data-filter=".wordpress">WORDPRESS</li>
-                </ul>
-            </div>
-
-            <div class="filters-content">
-                <div class="row portfolio-grid justify-content-center">
-                    <div class="col-lg-4 col-md-6 all cms">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p1.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p1.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="short_info">
-                                <h4>minimal design</h4>
-                                <p>Animated, portfolio</p>
+    
+                <div class="filters-content">
+                    <div class="row portfolio-grid justify-content-center">
+                        <div class="col-lg-4 col-md-6 all cms">
+                            <div class="portfolio_box">
+                                <div class="single_portfolio">
+                                    <img class="img-fluid w-100" src="prop/img/portfolio/php.png" alt="">
+                                    <div class="overlay"></div>
+                                    <a href="prop/img/portfolio/php.png" class="img-gal">
+                                        <div class="icon">
+                                            <span class="lnr lnr-cross"></span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="short_info">
+                                    <h4>The Comedy Store</h4>
+                                    <p>My Comey store is a store for comedy videos selling online.</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 all react">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p2.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p2.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="short_info">
-                                <h4>Paint wall</h4>
-                                <p>Animated, portfolio</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 all cms">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p3.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p3.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="short_info">
-                                <h4>female light</h4>
-                                <p>Animated, portfolio</p>
+                        <div class="col-lg-4 col-md-6 all react">
+                            <div class="portfolio_box">
+                                <div class="single_portfolio">
+                                    <img class="img-fluid w-100" src="prop/img/portfolio/2.png" alt="">
+                                    <div class="overlay"></div>
+                                    <a href="prop/img/portfolio/2.png" class="img-gal">
+                                        <div class="icon">
+                                            <span class="lnr lnr-cross"></span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="short_info">
+                                    <h4>Mealplan Website Basiligo</h4>
+                                    <p>Mealplan website front-end and backend - To manage meal plans for customers on weekly basis and cooking recipe management for cooks.</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 all react wordpress">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p4.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p4.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="short_info">
-                                <h4>fourth air</h4>
-                                <p>Animated, portfolio</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 all php wordpress">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p6.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p5.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="short_info">
-                                <h4>together sign</h4>
-                                <p>Animated, portfolio</p>
+                        <div class="col-lg-4 col-md-6 all cms">
+                            <div class="portfolio_box">
+                                <div class="single_portfolio">
+                                    <img class="img-fluid w-100" src="prop/img/portfolio/3.png" alt="">
+                                    <div class="overlay"></div>
+                                    <a href="prop/img/portfolio/3.png" class="img-gal">
+                                        <div class="icon">
+                                            <span class="lnr lnr-cross"></span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="short_info">
+                                    <h4>AB App</h4>
+                                    <p>Kids application for learning about Animals or Birds in their early ages. How they sound differently in different scenarios</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 all uix">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p5.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p6.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="short_info">
-                                <h4>multiply fowl</h4>
-                                <p>Animated, portfolio</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 all uix php">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p7.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p7.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="short_info">
-                                <h4>green heaven</h4>
-                                <p>Animated, portfolio</p>
+                        <div class="col-lg-4 col-md-6 all react wordpress">
+                            <div class="portfolio_box">
+                                <div class="single_portfolio">
+                                    <img class="img-fluid w-100" src="prop/img/portfolio/4.png" alt="">
+                                    <div class="overlay"></div>
+                                    <a href="prop/img/portfolio/4.png" class="img-gal">
+                                        <div class="icon">
+                                            <span class="lnr lnr-cross"></span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="short_info">
+                                    <h4>Web App Attigo</h4>
+                                    <p>Attigo - web application for Discover the right care you deserve
+                                        A react JS web application for COVID-19 survey</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 all php wordpress">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p8.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p8.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="short_info">
-                                <h4>fly male</h4>
-                                <p>Animated, portfolio</p>
+           
+                        <div class="col-lg-4 col-md-6 all uix php">
+                            <div class="portfolio_box">
+                                <div class="single_portfolio">
+                                    <img class="img-fluid w-100" src="prop/img/portfolio/7.png" alt="">
+                                    <div class="overlay"></div>
+                                    <a href="prop/img/portfolio/7.png" class="img-gal">
+                                        <div class="icon">
+                                            <span class="lnr lnr-cross"></span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="short_info">
+                                    <h4>allschoolapparel</h4>
+                                    <p>a shopping cart</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 all uix">
-                        <div class="portfolio_box">
-                            <div class="single_portfolio">
-                                <img class="img-fluid w-100" src="prop/img/portfolio/p9.jpg" alt="">
-                                <div class="overlay"></div>
-                                <a href="prop/img/portfolio/p9.jpg" class="img-gal">
-                                    <div class="icon">
-                                        <span class="lnr lnr-cross"></span>
-                                    </div>
-                                </a>
+                        <div class="col-lg-4 col-md-6 all php wordpress">
+                            <div class="portfolio_box">
+                                <div class="single_portfolio">
+                                    <img class="img-fluid w-100" src="prop/img/portfolio/12.png" alt="">
+                                    <div class="overlay"></div>
+                                    <a href="prop/img/portfolio/12.png" class="img-gal">
+                                        <div class="icon">
+                                            <span class="lnr lnr-cross"></span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="short_info">
+                                    <h4>GDPUK</h4>
+                                    <p>One Store Market is a wordpress blog which is dealing with promotions for the products , advertisement from affiliate programs. SEO work is there and project is going on and a long term SEO work.</p>
+                                </div>
                             </div>
-                            <div class="short_info">
-                                <h4>season face</h4>
-                                <p>Animated, portfolio</p>
+                        </div>
+                        <div class="col-lg-4 col-md-6 all uix">
+                            <div class="portfolio_box">
+                                <div class="single_portfolio">
+                                    <img class="img-fluid w-100" src="prop/img/portfolio/13.png" alt="">
+                                    <div class="overlay"></div>
+                                    <a href="prop/img/portfolio/13.png" class="img-gal">
+                                        <div class="icon">
+                                            <span class="lnr lnr-cross"></span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="short_info">
+                                    <h4>Colorado Dental Association</h4>
+                                    <p>CDA Online is a web site related to colorado dental association. Site is developed using CMS Joomla with MySQL. The main extensions used in this site are virtuemart , sobi2. The feature of the site is we integrate both of these components together to work with registration process..</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 all uix">
+                            <div class="portfolio_box">
+                                <div class="single_portfolio">
+                                    <img class="img-fluid w-100" src="prop/img/portfolio/9.png" alt="">
+                                    <div class="overlay"></div>
+                                    <a href="prop/img/portfolio/9.png" class="img-gal">
+                                        <div class="icon">
+                                            <span class="lnr lnr-cross"></span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="short_info">
+                                    <h4>Discount Medical Source</h4>
+                                    <p>Its a shopping cart using x-cart.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     <!-- Pricing End -->
 
 </div>
