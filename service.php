783c45a0294558f8b90533654956b11bb6323953
Baljeet Singh <ebsbaljeet4298@gmail.com>
2023-06-13 13:06:59 +0530

    c

---
+++ service.php
@@ -0,0 +1,411 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<meta name="robots" content="">
+	<meta name="description" content="CryptoZone - Crypto Trading HTML Template">
+	<meta property="og:title" content="CryptoZone - Crypto Trading HTML Template">
+	<meta property="og:description" content="CryptoZone - Crypto Trading HTML Template">
+	<meta property="og:image" content="">
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Title -->
+    <title>Services</title>
+
+	<!-- Favicon icon -->
+	<link rel="icon" type="image/png" href="images/favicon.png">
+	
+	<!-- Custom Stylesheet -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Google Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap">
+
+</head>
+<body>
+
+<!--*******************
+        Preloader start
+    ********************-->
+    <div id="loader">
+	</div>
+    <!--*******************
+        Preloader end
+    ********************-->
+	
+<div class="page-wraper">
+
+	<!-- Header -->
+	<header class="site-header mo-left header header-transparent style-1">
+
+		<!-- Main Header -->
+		<div class="sticky-header main-bar-wraper navbar-expand-lg">
+			<div class="main-bar clearfix">
+				<div class="container clearfix">
+
+					<!-- Website Logo -->
+					<div class="logo-header">
+						<a href="index.html" class="logo-dark"><img src="images/logo.png" alt=""></a>
+						<a href="index.html" class="logo-light"><img src="images/logo-white.png" alt=""></a>
+					</div>
+					
+					<!-- Nav Toggle Button Strat -->
+					<button class="navbar-toggler collapsed navicon justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+					<!-- Nav Toggle Button End -->
+					
+					<!-- Extra Nav Start -->
+					<div class="extra-nav">
+						<div class="extra-cell">
+							<a class="btn btn-outline-primary text-white" target="_blank" href="https://cryptozone.dexignzone.com/xhtml/page-login.html">Login</a>
+							<a class="btn btn-primary btn-gradient btn-shadow" target="_blank" href="https://cryptozone.dexignzone.com/xhtml/page-register.html">Register</a>
+						</div>
+					</div>
+					<!-- Extra Nav End -->
+						
+					<div class="header-nav navbar-collapse collapse justify-content-end" id="navbarNavDropdown">
+						
+						<!-- Mobile Sidebar Logo -->
+						<div class="logo-header mostion">
+							<a href="index.html" class="logo-dark"><img src="images/logo.png" alt=""></a>
+						</div>
+						
+						<!-- Navbar nav -->
+						<ul class="nav navbar-nav navbar">
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about-us.html">About Us</a></li>
+							<li><a href="pricing.html">Pricing</a></li>
+							<li class="sub-menu-down"><a href="javascript:void(0);">Blog</a>
+								
+							</li>
+							<li><a href="contact-us.html">Contact Us</a></li>
+						</ul>
+						
+						<!-- Mobile Sidebar bottom -->
+						<div class="header-bottom">
+							<div class="dz-social-icon">
+							  <ul>
+								<li>
+								  <a
+									target="_blank"
+									class="fab fa-facebook-f"
+									href="https://www.facebook.com/ebullientsoft"
+								  ></a>
+								</li>
+								<li>
+								  <a
+									target="_blank"
+									class="fab fa-twitter"
+									href="https://twitter.com/"
+								  ></a>
+								</li>
+								<li>
+								  <a
+									target="_blank"
+									class="fab fa-linkedin-in"
+									href="https://www.linkedin.com/in/ebullient-soft-818318158/"
+								  ></a>
+								</li>
+								<li>
+								  <a
+									target="_blank"
+									class="fab fa-instagram"
+									href="https://instagram.com/ebullientsoft?igshid=MjEwN2IyYWYwYw==/"
+								  ></a>
+								</li>
+							  </ul>
+							</div>
+						  </div>
+						
+					</div>
+					
+				</div>
+			</div>
+
+		</div>
+		<!-- Main Header End -->
+
+	</header>
+	<!-- Header -->
+	
+	
+	<!-- Page Content Start -->
+	<div class="page-content">
+			
+		<!-- Banner  -->
+		<div class="dz-bnr-inr style-1 text-center">
+
+		
+			<img class="bg-shape1" src="images/home-banner/shape1.png" alt="">
+			<img class="bg-shape2" src="images/home-banner/shape1.png" alt="">
+			<img class="bg-shape3" src="images/home-banner/shape3.png" alt="">
+			<img class="bg-shape4" src="images/home-banner/shape3.png" alt="">
+		
+		</div>
+		<!-- Banner End -->
+		
+		<!-- Blog Details -->
+		        <!-- Crypto From And Services Start -->
+                <section
+                class="content-inner bg-light icon-section section-wrapper2"
+                id="section1"
+                class="content-section"
+              >
+                <div class="container">
+                  <div class="section-head text-center">
+                    <h2 class="title">
+                      Our
+                      <span class="text-primary"> Services </span>
+                    </h2>
+                  </div>
+                  <div class="row sp60">
+                    <div
+                      class="col-xl-4 col-md-6 m-b60 wow fadeInUp"
+                      data-wow-delay="0.2s"
+                    >
+                      <div class="icon-bx-wraper style-3 text-center">
+                        <div class="icon-media">
+                          <img
+                            src="images/icons/undraw_dev_focus_re_6iwt.svg"
+                            alt=""
+                            width="100px"
+                          />
+                        </div>
+                        <div class="icon-content">
+                          <h4 class="title">Software Development</h4>
+                          <p class="m-b0">
+                          In software development, the best code is not just elegant and efficient; it tells a story. It narrates the vision, the dedication, and the collective intelligence of a team working towards building something truly remarkable.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      class="col-xl-4 col-md-6 m-b60 wow fadeInUp"
+                      data-wow-delay="0.4s"
+                    >
+                      <div class="icon-bx-wraper style-3 text-center">
+                        <div class="icon-media">
+                          <img
+                            src="images/icons/undraw_programmer_re_owql.svg"
+                            width="100px"
+                            alt=""
+                          />
+                        </div>
+                        <div class="icon-content">
+                          <h4 class="title">Web Development</h4>
+                          <p class="m-b0">
+                          Web development is a journey of transforming ideas into digital experiences. With every line of code, we weave together design, functionality, and user experience to create a virtual world that engages, empowers, and connects people across the globe.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      class="col-xl-4 col-md-6 m-b60 wow fadeInUp"
+                      data-wow-delay="0.6s"
+                    >
+                      <div class="icon-bx-wraper style-3 text-center">
+                        <div class="icon-media">
+                          <img
+                            src="images/icons/undraw_dev_productivity_re_fylf.svg"
+                            width="100px"
+                            alt=""
+                          />
+                        </div>
+                        <div class="icon-content">
+                          <h4 class="title">Application Maintainance</h4>
+                          <p class="m-b0">
+                          Application maintenance is the gentle art of balancing preservation and progress, ensuring that software systems evolve harmoniously to meet changing needs while preserving the integrity of what has already been built.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      class="col-xl-4 col-md-6 m-b60 wow fadeInUp"
+                      data-wow-delay="0.8s"
+                    >
+                      <div class="icon-bx-wraper style-3 text-center">
+                        <div class="icon-media">
+                          <img
+                            src="images/icons/undraw_my_code_snippets_re_4adu.svg"
+                            alt=""
+                            width="100px"
+                          />
+                        </div>
+                        <div class="icon-content">
+                          <h4 class="title">iOS/Android App Development</h4>
+                          <p class="m-b0">
+                          iOS and Android app development is the art of turning imagination into innovation, code into user experiences, and mobile devices into gateways of limitless possibilities.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      class="col-xl-4 col-md-6 m-b60 wow fadeInUp"
+                      data-wow-delay="1.0s"
+                    >
+                      <div class="icon-bx-wraper style-3 text-center">
+                        <div class="icon-media">
+                          <img
+                            src="images/icons/undraw_security_on_re_e491.svg"
+                            width="100px"
+                            alt=""
+                          />
+                        </div>
+                        <div class="icon-content">
+                          <h4 class="title">Web Security</h4>
+                          <p class="m-b0">
+                          Web security is not a destination; it's a continuous journey. Stay vigilant, anticipate threats, and fortify your defenses. Remember, the strength of your web security is measured by your commitment to protect, adapt, and evolve in the face of ever-changing threats.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      class="col-xl-4 col-md-6 m-b60 wow fadeInUp"
+                      data-wow-delay="1.0s"
+                    >
+                      <div class="icon-bx-wraper style-3 text-center">
+                        <div class="icon-media">
+                          <img
+                            src="images/icons/undraw_designer_life_re_6ywf.svg"
+                            alt=""
+                            width="100px"
+                          />
+                        </div>
+                        <div class="icon-content">
+                          <h4 class="title">Responsive Web Design</h4>
+                          <p class="m-b0">
+                          Responsive web design is not just about adjusting screen sizes; it's about adapting to the ever-changing digital landscape. It is the art of crafting seamless user experiences that effortlessly flow across devices, ensuring your message reaches every corner of the connected world.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                   
+                  </div>
+                </div>
+                <img
+                  class="bg-shape1"
+                  src="images/home-banner/shape1.png"
+                  alt=""
+                  width="60px"
+                />
+              </section>
+		<!-- Blog Details -->
+
+	</div>
+	<!-- Page Content End -->
+
+	<!-- Footer -->
+    <footer class="site-footer style-1" id="footer">
+        <img class="bg-shape1" src="images/home-banner/shape1.png" alt="" />
+
+        <div class="footer-top background-luminosity" style="background-image: url(images/background/bg1.jpg)">
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-4 col-lg-12 col-md-12 wow fadeInUp" data-wow-delay="0.2s">
+                <div class="widget widget_about">
+                  <div class="footer-logo logo-white">
+                    <a href="index.php"><img src="images/logo-white.png" alt="" width="80px" /></a>
+                  </div>
+                  <p></p>
+                  <div class="dz-social-icon transparent space-10">
+                    <ul>
+                      <li>
+                        <a target="_blank" class="fab fa-facebook-f" href="https://www.facebook.com/ebullientsoft"></a>
+                      </li>
+                      <li>
+                        <a target="_blank" class="fab fa-instagram" href="https://www.instagram.com/ebullientsoft/?igshid=MjEwN2IyYWYwYw%3D%3D%2F/"></a>
+                      </li>
+                      <li>
+                        <a target="_blank" class="fab fa-linkedin" href="https://www.linkedin.com/in/ebullient-soft-818318158/"></a>
+                      </li>
+                      <li>
+                        <a target="_blank" class="fab fa-youtube" href="https://youtube.com/"></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-2 col-lg-4 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.4s">
+                <div class="widget widget_services">
+                  <h4 class="widget-title">Other Links</h4>
+                  <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about-us.php">About Us</a></li>
+                    <li><a href="portfolio.php">Portfolio</a></li>
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="contact-us.php">Contact Us</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-xl-3 col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay="0.6s">
+                <div class="widget recent-posts-entry">
+                  <h4 class="widget-title">Blog Posts</h4>
+                  <div class="widget-post-bx">
+                    <div class="widget-post clearfix">
+                      <div class="dz-info">
+                        <h6 class="title">
+                          <a href="speech-recognition.php">What is Speech Recognition?</a>
+                        </h6>
+                        <span class="post-date"> FEB 3, 2021</span>
+                      </div>
+                    </div>
+                    <div class="post-separator"></div>
+                    <div class="widget-post clearfix">
+                      <div class="dz-info">
+                        <h6 class="title">
+                          <a href="ai-with-python.php">How can we create an Artificial Intelligence project with the use of Python?</a>
+                        </h6>
+                        <span class="post-date"> MAY 21, 2022</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-3 col-lg-4 col-sm-12 wow fadeInUp" data-wow-delay="0.8s">
+                <div class="widget widget_locations">
+                  <h4 class="widget-title">Locations</h4>
+                  <div class="clearfix">
+                    <h6 class="title">India</h6>
+                    <p>F-244, Phase 8-B, Industrial Area, Mohali, 160055</p>
+                    <img src="images/footer/world-map-with-flags1.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Footer Bottom Part -->
+        <div class="footer-bottom text-center">
+          <div class="container">
+            <span class="copyright-text">Copyright © 2022
+              <a href="https://ebullientsoft.com/" target="_blank">ebullientsoft</a>. All rights reserved.</span>
+          </div>
+        </div>
+      </footer>
+
+	<!-- Footer End -->
+
+	<!-- Bottom to top -->
+	<button class="scroltop icon-up" type="button" style="display: inline-block;"><i class="fas fa-arrow-up"></i></button>
+	
+</div>
+
+<!-- JAVASCRIPT FILES ========================================= -->
+<script src="js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script><!-- WOW.JS -->
+<script src="js/custom.js"></script><!-- CUSTOM JS -->
+</body>
+</html>
