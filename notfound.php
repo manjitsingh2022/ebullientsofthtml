3396dd5dde38811a37e945fd2c58991786c83475
Baljeet Singh <ebsbaljeet4298@gmail.com>
2023-06-14 10:44:02 +0530

    c

---
+++ notfound.php
@@ -0,0 +1,23 @@
+<?php require_once("header-inner.php"); ?>
+<!-- Page Content Start -->
+<div class="page-content">
+  <!-- Banner  -->
+  <div class="dz-bnr-inr style-1 text-center">
+    <div class="container">
+      <div class="dz-bnr-inr-entry">
+        <h1 class="text-center">Page Not Found</h1>
+        <p>The page you are looking for "<?php echo $_SERVER['REQUEST_URI']; ?>" could not be found.</p>
+     
+      </div>
+    </div>
+    <img class="bg-shape1" src="images/home-banner/shape1.png" alt="" />
+    <img class="bg-shape2" src="images/home-banner/shape1.png" alt="" />
+   
+  </div>
+
+  <section class="content-inner" style="background: white">
+   
+  </section>
+</div>
+
+<?php require_once("footer.php"); ?>
